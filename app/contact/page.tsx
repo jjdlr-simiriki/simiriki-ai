e517f4337149ca0965de95cbed7ac978a227bda9
JJ De La ROsa <jjdlr@simiriki.com>
2025-07-31 13:22:56 -0600

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,7 @@
 export default function ContactPage() {
   return (
     <div className="container mx-auto py-8">
-              <iframe
+             <iframe
   src="https://forms.office.com/Pages/ResponsePage.aspx?...&embed=true"
   frameBorder={0}
   marginWidth={0}
@@ -11,7 +11,6 @@ export default function ContactPage() {
   style=https://operator.chatgpt.com/c/688b904eae988190964ae04c24ed568b#cua_citation-%20border:%20'none',%20maxWidth:%20'100%',%20maxHeight:%20'100vh'%20
   allowFullScreen
 />
-    /iframe>
     </div>
   );
 }
