adf2f41ac8402b8a4f2df62d4d07e7925591cc26
JJ De La ROsa <jjdlr@simiriki.com>
2025-07-31 12:55:34 -0700

    Create route.ts

---
+++ app/api/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from 'next/server';
+import puppeteer from 'puppeteer';
+
+export async function GET(req: NextRequest) {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: 'new'
+  });
+  const page = await browser.newPage();
+  await page.goto('https://simiriki.com');
+  const title = await page.title();
+  await browser.close();
+  return NextResponse.json({ title });
+}
