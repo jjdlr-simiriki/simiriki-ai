70ba1a8439f4400aeb0933253011fb9441e9a43c
JJ De La ROsa <jjdlr@simiriki.com>
2025-08-11 03:37:54 -0600

    chore: fix build by updating postcss config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   // Add other config options as needed
 };
 
 export default nextConfig;
+
